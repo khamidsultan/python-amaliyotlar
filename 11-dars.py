9db792371ab2beb254ad6a86930d20c43a8b3f57
khamidsultan <93081521+khamidsultan@users.noreply.github.com>
2021-11-08 13:15:25 +0400

    Create 11-dars.py
    add

---
+++ 11-dars.py
@@ -0,0 +1,97 @@
+
+
+# =============================================================================
+# son=int(input("Juft son kiritng!\n>>>"))
+# if son%2==0:
+#   print("Rahmat! ")
+# else:
+#   print("Bu juft son emas!")
+# =============================================================================
+
+
+
+# =============================================================================
+# yosh=int(input("Yoshingiz nechida?"))
+# if (yosh<4 or yosh>64): 
+#   chipta=0
+# elif  yosh<18: 
+#   chipta=10000
+# else:
+#   chipta=20000
+# print(f"Sizga kirish {chipta} so\'m")
+# =============================================================================
+
+
+
+# =============================================================================
+# print("Ikkita son kiriting!")
+# son1=float(input("1-son:"))
+# son2=float(input("2-son:"))
+# if son1>son2:
+#   print(f"{son1}>{son2}")
+# elif son1==son2:
+#   print(f"{son1}={son2}")
+# else:
+#   print(f"{son1}<{son2}")
+# =============================================================================
+
+
+# =============================================================================
+# 
+# mahsulotlar=['shaftoli', 'anor', 'olma','gilos','banan','non','atir','somsa', 'kitob']
+# savat=[]
+# for n in range(5):
+#     savat.append(input(f"{n+1}-mahsulotni kiriting:"))
+#     
+# for n in savat:
+#   if n.lower() in mahsulotlar:
+#     print(f"{n} do\'konimizda bor")
+#   else:
+#     print(f"{n} do\'konimizda yo'q")
+# 
+# =============================================================================
+
+
+# =============================================================================
+# 
+# mahsulotlar=['shaftoli', 'anor', 'olma','gilos','banan','non','atir','somsa', 'kitob']
+# savat=[]
+# bor_mahsulotlar=[]
+# mavjud_emas=[]
+# 
+# 
+# for n in range(5):
+#   savat.append(input(f"Savatga {n+1}-mahsulotni kiriting:"))
+#   
+# for mahsulot in savat:
+#   if mahsulot in mahsulotlar:
+#     bor_mahsulotlar.append(mahsulot)
+#   else:
+#     mavjud_emas.append(mahsulot)
+# 
+# 
+# if mavjud_emas:
+#     print("Do\'konimizda quyidagi mahsulotlar yo\'q:")
+#     for mahsulot in mavjud_emas:
+#      print(mahsulot) 
+# else:
+#    print("Siz so\'ragan barcha mahsulotlar bor!")
+# =============================================================================
+
+# =============================================================================
+# 
+# foydalanuvchilar=['anvar','doston','ibrohim','ahmad','sulton']
+# tanlang=input("Yangi login tanlang:")
+# if tanlang.lower() in foydalanuvchilar:
+#   print("Login band, yangi login tanlang!")
+# else:
+#   print("Xush kelibsiz")
+# 
+# son=float(input("Istalgan butun son kiritng:"))
+# sonlar=list(range(2,11))
+# for n in sonlar:
+#   if son%n==0:
+#     print(f"{son} soni {n}ga qoldizsiz bo'linadi")
+# =============================================================================
+
+
