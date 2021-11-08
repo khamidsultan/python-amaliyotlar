a2cb5fa230071c17eabdf4eeab7c7b9aba863d27
khamidsultan <93081521+khamidsultan@users.noreply.github.com>
2021-11-08 12:39:45 +0400

    Create 10-dars.py

---
+++ 10-dars.py
@@ -0,0 +1,40 @@
+
+#Anvar aka Nazrullayevning Python asoslari 10-dars topshiriqlari:
+#7/11/2021-Universitetning IT darsida bajardim soat 9:38 bo'ldi
+
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#   if car.lower() == 'gm':
+#     print(car.upper())
+#   else: 
+#     print(car.title())
+
+# ism=input("Ismingizni kiriting:\n>>>")
+# if ism.lower() == 'admin':
+#   print(f"Xush kelibsiz, Admin! Foydalanuvchilar ro\'yxatini ko\'rasizmi?")
+# else:
+#   print(f"Xush kelibsiz, {ism.title()}!")
+
+# sonlarni teng yoki teng emasligini tekshirish
+# print("Ikkita teng son kiriting:")
+# son1=float(input("1-son:"))
+# son2=float(input("2-son:"))
+# if son1==son2: print("Ikkala son teng!")
+# else: print("Ikkala son teng emas!")
+
+
+# sonning musbat, manfiy yoki nolga teng ekanini tekshirish
+# print("Istalgan son kiriting:")
+# son=float(input(">>>"))
+# if son<0:
+#   print("Son manfiy!")
+# elif son==0:
+#   print("Son nolga teng!")
+# else: print("Son musbat")
+
+
+# son=float(input("Son kiritsangiz kubini hisoblaymiz:\n>>>"))
+# if son>0:
+#   print(son**3)
+# else:
+#   print("Musbat son kiriting!")
